3dbf6d22f7420c4660f1d1f1ffb1bd7c0d7fb29c
254guru <otienooluda@gmail.com>
2023-08-29 02:27:48 -0700

    rints the first x elements of a list and only integers.

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for item in my_list:
+            if isinstance(item, int) and count < x:
+                print("{:d}".format(item), end=' ')
+                count += 1
+        print()
+    except TypeError:
+        pass
+    return count
