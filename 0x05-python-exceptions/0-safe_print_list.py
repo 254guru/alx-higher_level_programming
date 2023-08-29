edeabf80f8a2c92e59b8e1abee1f39e8457675e4
254guru <otienooluda@gmail.com>
2023-08-29 02:56:14 -0700

    fix bug

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for item in my_list:
             if count < x:
-                print(item, end=' ')
+                print(item, end='')
                 count += 1
 
         print()
