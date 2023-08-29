3fe1a559b28bd891896650f616427e0cef8483fb
254guru <otienooluda@gmail.com>
2023-08-29 02:10:09 -0700

    fix bug

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,10 +6,9 @@ def safe_print_list(my_list=[], x=0):
             if count < x:
                 print(item, end=' ')
                 count += 1
-            else:
-                break
+
+        print()
     except TypeError:
         pass
     finally:
-        print("")
-    return count
+        return count
