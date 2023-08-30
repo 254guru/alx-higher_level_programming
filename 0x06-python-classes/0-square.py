8f16cbb94855ae3f38c201a651f49d5384919f20
254guru <otienooluda@gmail.com>
2023-08-30 05:47:51 -0700

    docstrings in python

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""
+This is a brief description of your module.
+
+Provide more detailed information about the purpose and usage of your module here.
+"""
+
 class Square:
     pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+    """
+    This is a brief description of your class.
+
+    Provide more detailed information about the purpose and usage of your class here.
+    """
+class Square:
+    def __init__(self, size):
+        """
+        This is a brief description of your method.
+
+        Provide more detailed information about the purpose and usage of your method here.
+        """
+        self.__size = size
