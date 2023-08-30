17436bacca544d2eca8f330ba4eb5ea21721f64e
254guru <otienooluda@gmail.com>
2023-08-30 05:55:04 -0700

    changes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""
+"""Square Class
+
 This is a brief description of your module.
 
-Provide more detailed information about the purpose and usage of your module here.
 """
 
 class Square:
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-    """
+    """Square Class
+
     This is a brief description of your class.
 
-    Provide more detailed information about the purpose and usage of your class here.
     """
 class Square:
     def __init__(self, size):
-        """
+        """__init__
+
         This is a brief description of your method.
 
         Provide more detailed information about the purpose and usage of your method here.
