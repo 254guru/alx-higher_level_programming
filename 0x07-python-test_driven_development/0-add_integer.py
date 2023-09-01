3638a9708f0230865d67d6e0745915e4020b8cb0
254guru <otienooluda@gmail.com>
2023-09-01 10:04:48 -0700

    function that adds two integer or float

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""add_integer"""
+
+
+def add_integer(a, b=98):
+    """
+    function that adds two integers or floats
+    Args:
+        a (number): Float or Number
+        b (:obj:number:optional): Optional Float or Integer
+
+    Raises: TypeError: If not float or integer
+
+    Returns Integer or float
+    """
+    if not(isinstance(a, int) or isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    if not (isinstance(b, int) or isinstance(b, float)):
+        raise TypeError("b must be an integer")
+
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+
+    return int(a + b)
