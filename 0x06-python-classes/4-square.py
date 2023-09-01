063e04f7441399f563beb29d4788c7b4b3addea3
254guru <otienooluda@gmail.com>
2023-09-01 04:12:20 -0700

    change to executable

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,54 +1,55 @@
 #!/usr/bin/python3
-"""Square Class"""
+""" Module 4-square """
 
 
 class Square:
     """
-    Attributes:
-    size (int): size of square
+        This class represents a square.
+        Attributes:
+            __size (int): The size of the square.
+        Method:
+            __init__ : Initializes a new Square instance with an optional size.
+            area(self): Returns the current square area.
+            size(self): Getter method to retrieve the size attribute.
+            size(self, value): Setter method to set the size attribute.
     """
     def __init__(self, size=0):
         """
-        The __init__ method initializes a new square instance with optimal size
-        Args:
-        size (int): size of square
-        Raises:
-        TypeError: if size is not int
-        ValueError: if size is less than 0
+            Initializes a new Square instance with an optional size.
+            Args:
+                size (int): The size of the square (default is 0).
+            Raises:
+                TypeError: If size is not an integer.
+                ValueError: If size is less than 0.
         """
-        self.size = size
-        """
-        Private instance attribute: Size
-        """
+        self.__size = size
+
     def area(self):
         """
-        returns current square area
+            Returns the current square area.
+            Returns:
+                int: The area of the square.
         """
         return self.__size ** 2
 
     @property
     def size(self):
-         """
-        Note:
-            Do not include the `self` parameter in the ``Args`` section.
-
-        Args:
-
-        Returns:
-            Size
+        """
+            Getter method to retrieve the size attribute.
+            Returns:
+            int: The size of the square.
         """
         return self.__size
 
     @size.setter
     def size(self, value):
-          """
-        Note:
-            Do not include the `self` parameter in the ``Args`` section.
-
+        """
+        Setter method to set the size attribute.
         Args:
-            value (int): value int
-        Returns:
-            Area
+            value (int): The new size of the square.
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
         """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
