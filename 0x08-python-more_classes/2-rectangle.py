1bebb0891434678b7f4c7b52a1d20a5a64387960
254guru <otienooluda@gmail.com>
2023-09-06 03:11:08 -0700

    fix pycodestyle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -2,6 +2,8 @@
 """
 Rectangle module
 """
+
+
 class Rectangle:
     """
     Write an empty class Rectangle that defines a rectangle:
@@ -67,4 +69,3 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
-
