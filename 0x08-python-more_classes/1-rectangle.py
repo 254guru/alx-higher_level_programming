31ea4494224cb4de456260c7ec7eac25c993aa00
254guru <otienooluda@gmail.com>
2023-09-06 02:44:20 -0700

    rectangle module

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+Rectangle module
+"""
+class Rectangle:
+    """
+    an empty rectangle class that defines a rectangle
+    
+    Args:
+    width (int): int
+    height (int): int
+
+    Raises:
+    ValueError: less than 0
+    TypeError: not an integer
+
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+        int: width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+         """
+        Args:
+           value (int): int
+        Raises:
+            TypeError: not int
+            ValueError: less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        int: height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+         """
+        Args:
+           value (int): int
+        Raises:
+            TypeError: not int
+            ValueError: less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
--- 0x08-python-more_classes/README.md
+++ 0x08-python-more_classes/README.md
@@ -1,2 +1,2 @@
 # More Classes and Objects
-This is a continuation of the topic <Classes and Objects>
+This is a continuation of the topic <0x06-python-classes> https://github.com/254guru/alx-higher_level_programming/tree/master/0x06-python-classes
