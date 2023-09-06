399e51dca5d18c5390c82e14e7abb45bcac4c3d3
254guru <otienooluda@gmail.com>
2023-09-06 05:52:00 -0700

    fix pycodestyle

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,10 +2,12 @@
 """
 locked class module
 """
+
+
 class LockedClass(object):
     """
-    a locked class module with no class object attribute, 
-    that prevents the user from dynamically creating new instance attributes, 
+    a locked class module with no class object attribute,
+    that prevents the user from dynamically creating new instance attributes,
     except if the new instance attribute is called first name.
     """
     __slots__ = 'first_name'
