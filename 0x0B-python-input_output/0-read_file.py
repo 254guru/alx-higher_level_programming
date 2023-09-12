0b85e823d0b973b82057de6fa7e79107f0431505
254guru <otienooluda@gmail.com>
2023-09-12 08:58:01 -0700

    fix bug

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -12,8 +12,3 @@ def read_file(filename=""):
         with open(filename, 'r', encoding='utf-8') as file:
             for line in file:
                 print(line, end='')
-
-    except FileNotFoundError:
-        print(f"File '{filename}' not found.")
-    except Exception as e:
-        print(f"An error occured: {str(e)}")
