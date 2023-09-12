eedf802066eb752e1cb3aa4f749c4059086b40c2
254guru <otienooluda@gmail.com>
2023-09-12 09:17:51 -0700

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,11 +8,5 @@ def read_file(filename=""):
     """
     function that reads a text file (UTF8) and prints it to stdout
     """
-    with open(filename, 'r', encoding='utf-8') as file:
-        for line in file:
-            print(line, end='')
-
-    except FileNotFoundError:
-        print(f"file '{filename}' not found.")
-    except Exception as e:
-        print(f"An error occured: {str(e)}")
+    with open(filename, mode='r', encoding='utf-8') as file:
+            print(file.read(), end='')
