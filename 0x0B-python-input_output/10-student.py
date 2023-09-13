84d8521796f6d47c425479d8a2a31f59f39a2f5b
254guru <otienooluda@gmail.com>
2023-09-13 03:34:53 -0700

    inherit from 9

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+class module
+"""
+
+
+class Student:
+    """
+    student class module
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        instantation
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves dictionary representation
+        """
+        my_list = self.__dict__
+        stringdict_ = dict()
+
+        if type(attrs) is list:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return my_list
+
+                if attr in my_list:
+                    stringdict_[attr] = my_list[attr]
+
+            return stringdict_
+
+        return stringdict_
