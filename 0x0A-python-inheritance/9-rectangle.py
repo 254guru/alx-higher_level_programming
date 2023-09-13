3d636003931c24e3a06fb73ce43f22635911f1a9
254guru <otienooluda@gmail.com>
2023-09-12 22:57:17 -0700

    inherit from 8

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+module
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    class module
+    """
+    def __init__(self, width, height):
+        """
+        inheritance function
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return '[Rectangle] ' + str(self.__width) + '/' + str(self.__height)
