a7c3d5d0370cd66cffbeb9572d0e67885101a7bf
254guru <otienooluda@gmail.com>
2023-09-12 23:31:36 -0700

    fix pycodestyle

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -9,14 +9,13 @@ class MyInt(int):
     myint module
     """
 
-
     def __eq__(self, other):
         """
         invert behavior of the == operator
         """
         return not super().__eq__(other)
 
-    def __ne__(self,other):
+    def __ne__(self, other):
         """
         invert the behavior of the != operator
         """
