8a743473d2dfc87e2cccf2b55f41d054bdddf95d
254guru <otienooluda@gmail.com>
2023-09-15 03:35:01 -0700

    base class module

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+import modules
+"""
+import json
+from os import path
+
+
+class Base:
+    """
+    base class for managing id attributes in derived classes.
+    """
+    __nb_objects = 0 # private class attribute
+
+    def __init__(self, id=None):
+        """
+        constructor for base class
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
