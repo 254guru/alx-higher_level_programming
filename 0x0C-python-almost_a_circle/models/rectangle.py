a01f33be34b9659a8a05f440655d8f295729a6d9
254guru <otienooluda@gmail.com>
2023-09-15 09:07:17 -0700

    update class rectangle by assigning arguments to each attribute

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -114,3 +114,12 @@ class Rectangle(Base):
         return '[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}'.format(
             self.id, self.x, self.y, self.width, self.height
         )
+
+    def update(self, *args):
+        """
+        updates attributes using arguments in order (id, width, height, x, y)
+        """
+        args_names = ["id", "width", "height", "x", "y"]
+        for i, arg in enumerate(args):
+            if i < len(arg_names):
+                setattr(self, arg_names[i], arg)
