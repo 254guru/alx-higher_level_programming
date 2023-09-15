01fc775448fab5c60f2917d679b56eb7dc95cfbb
254guru <otienooluda@gmail.com>
2023-09-15 06:45:11 -0700

    change location

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+import modules
+"""
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+import pep8
+
+
+class Test_rectangle(unittest.TestCase):
+    """
+    test class
+    """
+    def test_pep8_base(self):
+        """
+        checks pep8
+        """
+        syntax = pep8.StyleGuide(quit=True)
+        check = syntax.check_files(['models/rectangle.py'])
+        self.assertEqual(
+                check.total_errors, 0,
+                "Found code style errors (and warnings)"
+                )
+
+    def test_rectangle_subclass(self):
+        """
+        Test if Rectangle class inherit from
+        Base class
+        """
+        self.assertTrue(issubclass(Rectangle, Base))
+
+    def test_parameters(self):
+        """
+        Test parameters for Rectangle class
+        """
+        r1 = Rectangle(10, 2)
+        r2 = Rectangle(2, 10)
+        r3 = Rectangle(10, 2, 0, 0, 12)
+
+        self.assertEqual(r1.id, 4)
+        self.assertEqual(r1.width, 10)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.x, 0)
+        self.assertEqual(r1.y, 0)
+        self.assertEqual(r2.id, 5)
+        self.assertEqual(r2.width, 2)
+        self.assertEqual(r2.height, 10)
+        self.assertEqual(r2.x, 0)
+        self.assertEqual(r2.y, 0)
+        self.assertEqual(r3.id, 12)
+        self.assertEqual(r3.width, 10)
+        self.assertEqual(r3.height, 2)
+        self.assertEqual(r3.x, 0)
+        self.assertEqual(r3.y, 0)
+
+        with self.assertRaises(TypeError):
+            r4 = Rectangle()
