fff7e7aa708e6301165dcf2addbd861e43cf2289
254guru <otienooluda@gmail.com>
2023-09-18 07:49:05 -0700

    update class base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,3 +30,21 @@ class Base:
         if list_dictionaries is Nne or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        save a list of objects to ba json file
+        """
+        filename = cls.__name__ + '.json'
+
+        with open(filename, mode='w', encoding='utf-8') as f:
+            if list_objs is None:
+                return f.write(cls.to_json_string(None))
+
+            json_attrs = []
+
+            for elem in list_objs:
+                json_attrs.append(elem.to_dictionary())
+
+            return f.write(cls.to_json_string(json_attrs))
