b9822c8c116c34b5e6956d0c8d75c03e60b10139
254guru <otienooluda@gmail.com>
2023-11-19 10:29:09 -0800

    script that adds the State object “Louisiana” to the database

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,47 +1,28 @@
 #!/usr/bin/python3
 """
-import modules
+script that adds the State object “Louisiana”
+to the database hbtn_0e_6_usa
 """
 
-import sys
+from sys import argv
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-
-
-def add_state(username, password, database):
-    """
-    add a new state object to the database
-    """
-    # Create a database engine
-    engine = create_engine(f'mysql://{username}:{password}@localhost:3306/{database}')
-
-    # Bind the engine to the Base class
-    Base.metadata.bind = engine
-
-    # Create a session
-    DBSession = sessionmaker(bind=engine)
-    session = DBSession()
-
-    # Create a new State object
-    new_state = State(name="Louisiana")
-
-    # Add the new State object to the session
-    session.add(new_state)
-    session.commit()  # Commit changes to the database to persist the new state
-
-    # Print the newly created state's ID
-    print(new_state.id)
-
-    # Close the session
-    session.close()
 
 if __name__ == "__main__":
     """
-    execute program
+    Access the database and get a state
     """
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <database>")
-    else:
-        username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-        add_state(username, password, database)
+
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    session.commit()
+    print('{0}'.format(new_state.id))
+    session.close()
