c37c9bf72a93127428527381f25d35fecd0850d4
254guru <otienooluda@gmail.com>
2023-11-20 04:39:57 -0800

    rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
+
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+    if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
