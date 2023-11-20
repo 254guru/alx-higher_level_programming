b3d776e78d6c20e92292e9306d30cb2edc066280
254guru <otienooluda@gmail.com>
2023-11-20 05:30:24 -0800

    fix semistandard

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,26 +3,26 @@
 const Rectangle = require('./5-square.js');
 
 module.exports = class Square extends Rectangle {
-    constructor(size) {
-        super(size, size);
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
     }
 
-    charPrint(c) {
-        if (c === undefined) {
-            c = 'X';
-        }
-
-        if (!this.width || !this.height) {
-            console.log("Invalid dimensions for the square.");
-            return;
-        }
-
-        for (let i = 0; i < this.height; i++) {
-            let row = '';
-            for (let j = 0; j < this.width; j++) {
-                row += c;
-            }
-            console.log(row);
-        }
+    if (!this.width || !this.height) {
+      console.log('Invalid dimensions for the square.');
+      return;
     }
-}
+
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += c;
+      }
+      console.log(row);
+    }
+  }
+};
