279d4dce12114a0908f0dfd71e0cc0b76206cb75
254guru <otienooluda@gmail.com>
2023-12-01 08:38:33 -0800

    revert commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,18 +3,17 @@
 fetches a link
 """
 
-
-from urllib.request import Request, urlopen
+import urllib.request
 
 
 if __name__ == "__main__":
-    req = Request('https://intranet.hbtn.io/status')
+    url = 'https://alx-intranet.hbtn.io/status'
+    req = urllib.request.Request(url)
 
-    with urlopen(req) as res:
-        content = res.read()
-        utf8_content = content.decode('utf-8')
+    with urllib.request.urlopen(req) as response:
+        body = response.read().decode('utf-8')
 
-        print('Body response:')
-        print('\t- type: {_type}'.format(_type=type(content)))
-        print('\t- content: {_content}'.format(_content=content))
-        print('\t- utf8 content: {_utf8_c}'.format(_utf8_c=utf8_content))
+        # Displaying the body of the response with tabulation before each line
+        lines = body.split('\n')
+        for line in lines:
+            print(f"\t- {line}")
