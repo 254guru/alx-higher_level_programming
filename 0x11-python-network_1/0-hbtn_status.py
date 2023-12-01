9f55a6bb32b50940b574feac8d71462aacc6ba59
254guru <otienooluda@gmail.com>
2023-12-01 08:33:12 -0800

    fix bug

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,11 +4,17 @@ fetches a link
 """
 
 
+from urllib.request import Request, urlopen
+
+
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print(f"\t- content: {html}")
-        print("\t- utf8 content: {}".format(html.decode("utf-8")))
+    req = Request('https://intranet.hbtn.io/status')
+
+    with urlopen(req) as res:
+        content = res.read()
+        utf8_content = content.decode('utf-8')
+
+        print('Body response:')
+        print('\t- type: {_type}'.format(_type=type(content)))
+        print('\t- content: {_content}'.format(_content=content))
+        print('\t- utf8 content: {_utf8_c}'.format(_utf8_c=utf8_content))
