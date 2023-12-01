bcf1470e7127f4d090cbc62a47ed9afbfff78c4c
254guru <otienooluda@gmail.com>
2023-12-01 08:27:51 -0800

    fix bugs

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,14 +6,15 @@ fetches a link
 
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
 
-req = urllib.request.Request(url)
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    req = urllib.request.Request(url)
 
-with urllib.request.urlopen(req) as response:
-    body = response.read().decode('utf-8')
+    with urllib.request.urlopen(req) as response:
+        body = response.read().decode('utf-8')
 
-    # Displaying the body of the response with tabulation before each line
-    lines = body.split('\n')
-    for line in lines:
-        print(f"\t- {line}")
+        # Displaying the body of the response with tabulation before each line
+        lines = body.split('\n')
+        for line in lines:
+            print(f"\t- {line}")
