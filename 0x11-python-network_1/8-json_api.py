90446aeb5495a6f544912e221c3dd48328fe85c3
254guru <otienooluda@gmail.com>
2023-12-02 05:37:40 -0800

    fix pycodestyle and bugs

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,21 +7,21 @@ import requests
 import sys
 
 
-if len(sys.argv) < 2:
-    q = ""
-else:
-    q = sys.argv[1]
-
-url = 'http://0.0.0.0:5000/search_user'
-data = {'q': q}
-
-response = requests.post(url, data=data)
-
-try:
-    json_response = response.json()
-    if json_response:
-        print(f"[{json_response['id']}] {json_response['name']}")
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        q = ""
     else:
-        print("No result")
-except ValueError:
-    print("Not a valid JSON")
+        q = sys.argv[1]
+
+        url = 'http://0.0.0.0:5000/search_user'
+        data = {'q': q}
+
+        response = requests.post(url, data=data)
+        try:
+            json_response = response.json()
+            if json_response:
+                print(f"[{json_response['id']}] {json_response['name']}")
+            else:
+                print("No result")
+        except ValueError:
+            print("Not a valid JSON")
