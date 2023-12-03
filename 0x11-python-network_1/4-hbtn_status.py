16f7c45631d5612a2f29ed7df4757cc9bf7eabf6
254guru <otienooluda@gmail.com>
2023-12-03 05:57:35 -0800

    set to expected output

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -10,9 +10,8 @@ url = 'https://alx-intranet.hbtn.io/status'
 response = requests.get(url)
 
 if response.status_code == 200:
-    body = response.text
-    lines = body.split('\n')
-    for line in lines:
-        print(f"\t- {line}")
+    print('Body response:')
+    print('\t- type: {_type}'.format(_type=type(response.text)))
+    print('\t- content: {_content}'.format(_content=response.text))
 else:
     print(f"Failed to fetch the URL. Status code: {response.status_code}")
