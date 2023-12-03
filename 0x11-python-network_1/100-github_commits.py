88ebdb297831da0c0772dedea41bcd47f8ff8590
254guru <otienooluda@gmail.com>
2023-12-03 06:24:45 -0800

    fetch commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,20 +9,24 @@ import requests
 import sys
 
 
+def get_commits(repo_name, owner_name):
+    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print("Error:", response.json())
+        return
+
+    commits = response.json()
+    for commit in commits[:10]:
+        sha = commit['sha']
+        author_name = commit['commit']['author']['name']
+        print(f"{sha}: {author_name}")
+
 if __name__ == "__main__":
-    commit_format = "{}: {}"
-    api_url = "https://api.github.com/repos/{}/{}/commits"
-    formatted_url = api_url.format(argv[2], argv[1])
-
-    response = requests.get(formatted_url)
-
-    if response.status_code == 200:
-        count = 0
-        for commit in response.json():
-            if count < 10:
-                sha = commit.get("sha")
-                author_name = commit.get("commit").get("author").get("name")
-                print(commit_format.format(sha, author_name))
-                count += 1
+    if len(sys.argv) != 3:
+        print("Usage: python script_name.py repository_name owner_name")
     else:
-        print("Failed to fetch commits. Check repository and owner names.")
+        repo_name = sys.argv[1]
+        owner_name = sys.argv[2]
+        get_commits(repo_name, owner_name)
